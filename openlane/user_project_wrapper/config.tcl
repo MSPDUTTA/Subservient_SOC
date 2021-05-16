a1102cd27eb2409a88073304c538eb447011cad7
Priyanka Dutta <78390236+MSPDUTTA@users.noreply.github.com>
2021-05-15 20:38:14 -0700

    Update config.tcl

--- openlane/user_project_wrapper/config.tcl
+++ openlane/user_project_wrapper/config.tcl
@@ -42,7 +42,7 @@ set ::env(MACRO_PLACEMENT_CFG) $script_dir/macro.cfg
 ### Black-box verilog and views
 set ::env(VERILOG_FILES_BLACKBOX) "\
 	$script_dir/../../caravel/verilog/rtl/defines.v \
-	$script_dir/../../verilog/rtl/subservient_top_module.v \
+	$script_dir/../../verilog/rtl/subservient/subservient_top_module.v \
 	$script_dir/../../verilog/rtl/sky130_sram_1kbyte_1rw1r_32x256_8.v"
 	
 
