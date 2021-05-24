4c7f1f9aee28ad9671c5da503774b83a35f1bce2
Priyanka Dutta <78390236+MSPDUTTA@users.noreply.github.com>
2021-05-24 16:20:37 -0700

    Update config.tcl

--- openlane/subservient/config.tcl
+++ openlane/subservient/config.tcl
@@ -46,7 +46,8 @@ set ::env(FP_SIZING) absolute
 set ::env(DIE_AREA) "0 0 900 600"
 set ::env(DESIGN_IS_CORE) 0
 
-set ::env(PDN_CFG) $script_dir/pdn.tcl
+set ::env(VDD_NETS) [list {vccd1} {vccd2} {vdda1} {vdda2}]
+set ::env(GND_NETS) [list {vssd1} {vssd2} {vssa1} {vssa2}]
 
 set ::env(FP_PIN_ORDER_CFG) $script_dir/pin_order.cfg
 
